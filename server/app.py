a94ed89a60e7d885651d9d8a27d8bd7f61d19ade
Oizag <115131625+Oizag@users.noreply.github.com>
2024-04-24 16:22:24 +0300

    feat: add models

--- server/app.py
+++ server/app.py
@@ -1,19 +1,21 @@
-from flask import (
-    Flask,
-    render_template
-)
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from models import db
 import os
 
 # -- Конфигурация --
-app = Flask(__name__, static_url_path='/static')
-app.config.from_pyfile(f'{os.getcwd()}/server/config.py')
-db = SQLAlchemy()
+app = Flask(__name__, static_url_path="/static")
+app.config.from_pyfile(f"{os.getcwd()}/server/config.py")
 db.init_app(app)
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=8000, debug=True)
+    if not os.path.exists(f"{os.getcwd()}/instance/{app.config['DATABASE_FILE']}"):
+        with app.app_context():
+            db.create_all()
+    app.run("0.0.0.0", port=8000, debug=True)
---
+++ server/models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Cur(db.Model):
+    __tablename__ = "cur"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    cur_parameter = db.relationship("CurParameter", backref=db.backref("cur"))
+
+
+class CurParameter(db.Model):
+    __tablename__ = "cur_parameter"
+    id = db.Column(db.Integer, primary_key=True)
+    cur_id = db.Column(db.Integer, db.ForeignKey("cur.id"))
+    name = db.Column(db.String(255), nullable=False)
+    cur_parameter_value = db.relationship("CurParameterValue", backref=db.backref("Cur_parameter"))
+    
+class CurParameterValue(db.Model):
+    __tablename__ = 'cur_parameter_value'
+    id = db.Column(db.Integer, primary_key=True)
+    cur_parameter_id = db.Column(db.Integer, db.ForeignKey('cur_parameter.id'))
+    year = db.Column(db.Integer, nullable=False)
+    month = db.Column(db.Integer, nullable=False)
+    value = db.Column(db.Numeric(precision=15, scale=2), nullable=False)
