4e6b16f7b3a216a863a5313620d99c58efc5aefe
Valter Frank <glup0st1@mail.ru>
2024-04-26 01:16:13 +0300

    feat: add cur choice panel

--- server/app.py
+++ server/app.py
@@ -4,9 +4,11 @@ from flask import (
     request
 )
 from flask_sqlalchemy import SQLAlchemy
-from models import db
+from models import *
 from fill_db import fill_db
-import os
+from services import CURManager
+from collections import defaultdict
+import os, re
 
 # -- Конфигурация --
 app = Flask(__name__, static_url_path="/static")
@@ -24,7 +26,26 @@ def predict():
     if request.method == "POST":
         pass
 
-    return render_template("predict.html")
+    list_cur = [{
+        "name": item.name,
+        "id":item.id
+    } for item in Cur.query.all()]
+
+    list_indicators = [{
+        "name": re.sub(r'\W+', ' ', item.name),
+        "indicator_id": item.id
+    } for item in Indicator.query.all()]
+
+    check_list = []
+
+    result = defaultdict(list)
+
+    for cur_id, indicator_id in db.session.query(cur_indicator).all():
+        result[cur_id].append(indicator_id)
+
+    test = [{"cur_id": cur_id, "indicator_id": indicator_ids} for cur_id, indicator_ids in result.items()]
+
+    return render_template("predict.html", list_cur=list_cur, list_indicators=list_indicators, check_data=test)
 
 
 if __name__ == "__main__":
