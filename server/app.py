87e4b5d541c31f423ba0fb343e790672044bd32a
meowsl <vlafrank@ya.ru>
2024-04-26 15:07:54 +0300

    fix: debug mod

--- server/app.py
+++ server/app.py
@@ -95,7 +95,7 @@ def add_cors_headers(response):
 
 if __name__ == "__main__":
     init_db()
-    app.run("0.0.0.0", port=8000, debug=True)
+    app.run("0.0.0.0", port=8000)
 
 else:
     init_db()
